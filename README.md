99dd5187ed8f78ccb1ed196ce993975a4c68fa8e
vericamitreva <mitrevav98@gmail.com>
2024-06-04 20:16:35 +0200

    Documentation Added

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
-# React + Vite
+# Front-End Exercises
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This repository contains the solutions for the tasks provided. For now only the solution for the first task, creating a Reusable Button Component.
 
-Currently, two official plugins are available:
+## Built With
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- JavaScript
+- React.js
 
-# front-end-exercises
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+You need npm to be installed on your local machine. For details please use the following site: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+
+### Setup
+
+Next you need to clone the repository on your local machine with the following command line: git clone https://github.com/vericamitreva/front-end-exercises
+
+### Install
+
+To install the project you need to enter the directory you've cloned. Open a terminal and follow these next steps:
+
+npm install (to install all the necessary modules and dependencies)
+
+### Usage
+
+To run this project localy simply run the following line:
+
+npm run dev
+
+And the project should be deployed on your machine and you can access it on a browser by default on the URL: http://localhost:5173/
+
+## Author
+
+Verica Mitreva
+
+- GitHub: [@githubhandle](https://github.com/vericamitreva)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/vericamitreva)
--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,6 +1,24 @@
 import PropTypes from "prop-types"
 import { Button } from "antd"
 
+/**
+ * The ButtonComponent is a reusable button component that is built using the Ant Design library. It supports various customization options through props.
+ * Description of the props:
+ * @param {string} type - Specifies the type of the button ("outlined", "filled", "icon"). Default value is "default".
+ * @param {string} size - Specifies size of the button ("small", "medium", "large"). Default value is "default".
+ * @param {string} color - Specifies the background color of the button. Default value is "".
+ * @param {boolean} disabled - Specifies if the button is disabled. Default value is false.
+ * @param {boolean} loading - Specifies if the button is loading (showing the spinner inside the button). Defaults to false.
+ * @param {element} icon - Contains the icon that will be included in the button next to the content.
+ * @param {string} iconPosition - Specifies the position of the icon ("left", "right"). Default value is "left".
+ * @param {node} children - Contains the content inside the button. This prop is required.
+ * @param {Function} onClick - Function that will be called when the button is clicked.
+ * @param {Object} props - Contains any additional properties passed to the ButtonComponent.
+ * @example <ButtonComponent type="outlined" size="large" icon={<Icon/>} iconPosition="right" onClick={() => alert("Button clicked!")}>
+ *   Click me!
+ * </ButtonComponent>
+ */
+
 export default function ButtonComponent({ type, size, color, disabled, loading, icon, iconPosition, onClick, children, ...props }) {
 
     function buttonType(type) {
@@ -29,19 +47,23 @@ export default function ButtonComponent({ type, size, color, disabled, loading, 
         }
     }
 
+    function getIconPosition(iconPosition) {
+        return (iconPosition === "left" ? "start" : "end")
+    }
+
     return (
         <Button
             type={buttonType(type)}
             size={buttonSize(size)}
             style={{ background: color }}
+            iconPosition={getIconPosition(iconPosition)}
             disabled={disabled}
             loading={loading}
             onClick={onClick}
             {...props}
         >
-            {icon && iconPosition === "left" && icon}
+            {icon}
             {children}
-            {icon && iconPosition === "right" && icon}
         </Button>
     )
 }
@@ -59,9 +81,11 @@ ButtonComponent.propTypes = {
 }
 
 ButtonComponent.defaultProps = {
-    onClick: () => {},
+    type: "default",
+    size: "default",
+    color: "",
     iconPosition: "left",
     disabled: false,
     loading: false,
-    color: ""
+    onClick: () => {}
 }
