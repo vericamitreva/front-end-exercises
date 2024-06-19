f67a8689d698f33458a8c7d6736cecfb17c73139
vericamitreva <mitrevav98@gmail.com>
2024-06-19 13:49:57 +0200

    ModalComponent in TypeScript Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Icon from "./assets/react.jsx"
 import { Col, Row } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
 import { useState } from 'react'
-import ModalComponent from './components/ModalComponent/ModalComponent.jsx'
+import ModalComponent from './components/ModalComponent/ModalComponent.tsx'
 import CardComponent from './components/CardComponent/CardComponent.jsx'
 import cardData from "./assets/data/data.js"
 import ButtonComponent from './components/ButtonComponent/ButtonComponent.tsx'
